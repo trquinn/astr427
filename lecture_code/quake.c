6aac7fe4f843f3a240d6e5adf88d74d70bf8855c
Tom Quinn <trq@astro.washington.edu>
2025-10-28 21:16:57 -0700

    Added "quake" code from 10/28 discussion
    Added orbit integration example "toomre.ipynb"

---
+++ lecture_code/quake.c
@@ -0,0 +1,21 @@
+//  Code from "Quake" computer game to rapidly calculate an
+//  approximation to 1/sqrt(number)
+//  This is useful in lighting calculations, but also for
+//  gravitational calculations.
+//
+float Q_rsqrt( float number )
+{
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y  = number;
+	i  = * ( long * ) &y;                       // evil floating point bit level hacking
+	i  = 0x5f3759df - ( i >> 1 );               // Whiskey Tango ...
+	y  = * ( float * ) &i;
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+	return y;
+}
